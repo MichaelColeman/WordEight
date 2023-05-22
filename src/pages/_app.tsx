9ed3dfc2e0d78af88d330b41a96c55b694e9d14c
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-05-22 16:39:53 -0700

    add framework for app including base components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+/*
+The default export of _app.js is a top-level React component that wraps all the pages in your application.
+You can use this component to keep state when navigating between pages,
+or to add global styles as we're doing here.
+*/
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
-import Image from 'next/image'
-import { Inter, Hepta_Slab } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-const heptaSlab = Hepta_Slab({ subsets: ['latin'] })
+import Header from "../components/Header";
+import Board from "../components/Board";
+import Keyboard from "../components/Keyboard";
 
 export default function Home() {
   return (
     <>
-      <header className="h-16 border-b-2 flex items-center justify-center">
-        <h1 className={`text-2xl font-semibold text-white ${heptaSlab.className}`}>AARDVARK</h1>
-      </header>
-      <main className={`${inter.className}`}>yo</main>
+      <Header />
+      <Board />
+      <Keyboard />
     </>
-  )
+  );
 }
