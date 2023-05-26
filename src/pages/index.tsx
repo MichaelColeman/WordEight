8b3b1ea2b351ed4aaea9b1c58c35c1a271ea218b
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-05-26 15:03:59 -0700

    implement header and game components to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Inter, Hepta_Slab } from "next/font/google";
+import Header from "@/components/Header";
+import Aardvark from "@/components/Aardvark";
 
 const inter = Inter({ subsets: ["latin"] });
 const heptaSlab = Hepta_Slab({ subsets: ["latin"] });
@@ -22,14 +24,8 @@ export default function Home() {
 
   return (
     <>
-      <header className="h-16 border-2 text-white">
-        <h1
-          className={`font-semi-bold text-2xl text-white ${heptaSlab.className} border-gray-500`}
-        >
-          AARDVARK
-        </h1>
-      </header>
-      <main className={`${inter.className}`}>yo</main>
+      <Header />
+      <Aardvark solution={solution} />
     </>
   );
 }
