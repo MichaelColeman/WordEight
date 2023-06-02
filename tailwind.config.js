6611b9b8bb8658e8c1dde47398a4b17dab1233e8
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-02 03:47:27 -0700

    consolidate css, extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        'color-present:': colors.yellow[600],
-        'color-correct': colors.green[700],
-        'color-absent': colors.gray[700],
-        'color-tile-text': colors.gray[50],
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            animation: {
+                pop: "pop 0.3s ease-in-out forward",
+            },
+            keyframes: {
+                pop: {
+                    "0%, 100%": { transform: "scale(1)" },
+                    "20%": { transform: "scale(1.5)" },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
