c15249a5df936fea5836ed28a57f96ec2631a96e
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-02 04:37:14 -0700

    refactor typescript

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Row from "@/components/Row";
-import styles from "./Board.module.css";
 
-type BoardProps = {
+interface BoardProps {
     currentGuess: string;
     formattedGuesses: formattedGuesses;
     turn: number;
-};
+}
 
-const Board = ({ currentGuess, formattedGuesses, turn }: BoardProps) => {
+const Board: React.FunctionComponent<BoardProps> = ({ currentGuess, formattedGuesses, turn }) => {
     console.table(formattedGuesses);
     return (
         <div className={`flex flex-col gap-1`}>
