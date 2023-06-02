d2a5995637d2eec0faa40ea25a49b60e1610f536
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-02 03:44:31 -0700

    add animation delay prop to letter tiles

--- global.d.ts
+++ global.d.ts
@@ -1,18 +1,19 @@
 export {};
 
 declare global {
-  type formattedLetter = {
-    key: string;
-    result: string;
-  };
+    type formattedLetter = {
+        key: string;
+        result: string;
+        animationDelay: string;
+    };
 
-  type formattedGuess = Array<formattedLetter>;
+    type formattedGuess = Array<formattedLetter>;
 
-  type formattedGuesses = Array<formattedGuess>;
+    type formattedGuesses = Array<formattedGuess>;
 
-  interface solutionProps {
-    solution: string;
-  }
+    interface solutionProps {
+        solution: string;
+    }
 
-  type solutionArray = (string | null)[];
+    type solutionArray = (string | null)[];
 }
