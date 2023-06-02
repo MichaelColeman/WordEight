6795225b05fe57468b625581a16137985464a74c
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-02 03:38:19 -0700

    add tailwind and erase component css module

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -11,7 +11,7 @@ type BoardProps = {
 const Board = ({ currentGuess, formattedGuesses, turn }: BoardProps) => {
     console.table(formattedGuesses);
     return (
-        <div className={styles.board}>
+        <div className={`flex flex-col gap-1`}>
             {formattedGuesses.map((formattedGuess, i) => {
                 if (turn === i) {
                     return <Row key={i} formattedGuess={formattedGuess} currentGuess={currentGuess} />;
