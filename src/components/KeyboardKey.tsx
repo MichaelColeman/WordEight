42b8019ec3c9d517bdca61edc4fce13ea7daaf76
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-08 02:03:45 -0700

    rename variable

--- src/components/KeyboardKey.tsx
+++ src/components/KeyboardKey.tsx
@@ -4,7 +4,7 @@ type KeyboardKeyProps = {
 };
 
 const KeyboardKey = ({ data, result }: KeyboardKeyProps) => {
-  const handleGameKeyboardInput = (data: string) => {
+  const handleClick = (data: string) => {
     console.log(data);
     const keyboardEvent: KeyboardEvent = new KeyboardEvent("keyup", { key: data });
     window.dispatchEvent(keyboardEvent);
@@ -12,7 +12,7 @@ const KeyboardKey = ({ data, result }: KeyboardKeyProps) => {
 
   return (
     <button
-      onClick={() => handleGameKeyboardInput(data)}
+      onClick={() => handleClick(data)}
       className={`${result} flex-grow rounded bg-gray-500 font-bold uppercase text-white`}
       key={data}
     >
