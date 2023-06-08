dd8b472294b23a2d53faa4aa335797be07fc4a6d
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-08 05:23:09 -0700

    change modal style

--- src/components/EndGameModal.tsx
+++ src/components/EndGameModal.tsx
@@ -6,21 +6,23 @@ type props = {
 
 const EndGameModal = ({ isCorrect, turn, solution }: props) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40">
-      <div className="flex h-[200px] w-[75vw] items-center justify-center rounded-lg bg-green-700 bg-opacity-80 md:w-[400px]">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="flex h-[200px] w-[75vw] items-center justify-center rounded-lg bg-slate-700 bg-opacity-95 md:w-[400px]">
         {isCorrect && (
           <div className="flex flex-col items-center gap-2 ">
-            <h1 className="text-3xl uppercase text-green-200">You Win</h1>
-            <p className="solution text-lg text-gray-300 underline ">The word was {solution}</p>
-            <p className="text-lg">You have won in {turn} Guesses!</p>
+            <h1 className=" text-3xl font-bold uppercase tracking-wider text-green-500">You Win</h1>
+            <div className=" w-[90%] border-b border-emerald-300"></div>
+            <p className="text-base">You have won in {turn} Guesses!</p>
+            <p className="solution text-lg tracking-widest text-emerald-300 ">{solution.toUpperCase()}</p>
           </div>
         )}
 
         {!isCorrect && (
-          <div className=" flex flex-col items-center gap-2">
-            <h1 className="text-lg text-blue-500 ">Oh No..</h1>
-            <p className="solution text-base text-gray-300">You have used up all of your guesses!</p>
-            <p>Check back tomorrow for a new word, or refresh the page to try again!</p>
+          <div className="flex flex-col items-center gap-2 ">
+            <h1 className=" text-3xl font-bold uppercase tracking-wider text-rose-500">sorry!</h1>
+            <div className=" w-[90%] border-b border-rose-300"></div>
+            <p className="text-base">You have used up all of your guesses!</p>
+            <p className="solution text-lg tracking-widest text-rose-300 ">{solution.toUpperCase()}</p>
           </div>
         )}
       </div>
@@ -29,3 +31,9 @@ const EndGameModal = ({ isCorrect, turn, solution }: props) => {
 };
 
 export default EndGameModal;
+{
+  /* <p className="solution text-base text-gray-300">You have used up all of your guesses!</p> */
+}
+{
+  /* <p>Check back tomorrow for a new word, or refresh the page to try again!</p> */
+}
