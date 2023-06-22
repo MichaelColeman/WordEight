db0c9fd46e57dc25b31e65b6871d97c524fd8a5d
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-06-21 20:36:04 -0700

    implement game " how to play" modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import { Hepta_Slab } from "next/font/google";
+import Image from "next/image";
+import helpcircle from "@/images/helpcircle.svg";
+import PlayGuideModal from "@/components/PlayGuideModal";
 
 const heptaSlab = Hepta_Slab({ subsets: ["latin"] });
 
 export default function Header() {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleButtonClick = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <header className="mb-4 flex h-10 items-center justify-center border-b-2">
-      <h1 className={`text-lg font-semibold text-amber-400 md:text-2xl ${heptaSlab.className}`}>AARDVARK</h1>
-    </header>
+    <>
+      <header className="flex h-24 w-full justify-center border-b px-10">
+        <div className="flex-auto basis-2/6"></div>
+        <div className="flex-auto basis-2/6 self-center">
+          <h1 className="text-center text-2xl font-semibold text-amber-400 md:text-2xl">AARDVARK</h1>
+        </div>
+        <div className="flex flex-auto basis-2/6 justify-end">
+          <div className="flex">
+            <button onClick={handleButtonClick}>
+              <Image
+                src={helpcircle}
+                height={48}
+                width={48}
+                alt="how to play"
+                className="rounded-full hover:bg-slate-500"
+              />
+            </button>
+          </div>
+        </div>
+      </header>
+      {showModal && <PlayGuideModal handleButtonClick={handleButtonClick} />}
+    </>
   );
 }
---
+++ src/components/PlayGuideModal.tsx
@@ -0,0 +1,53 @@
+import { Dispatch, SetStateAction, MouseEvent } from "react";
+import roundedCloseButton from "@/images/Material-icons-round-cancel.svg";
+import modalExampleOne from "@/images/modal-example-1.png";
+import modalExampleTwo from "@/images/modal-example-2.png";
+import modalExampleThree from "@/images/modal-example-3.png";
+import Image from "next/image";
+
+type props = {
+  handleButtonClick: () => void;
+};
+
+const PlayGuideModal = ({ handleButtonClick }: props) => {
+  return (
+    <div
+      onClick={handleButtonClick}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 text-white"
+    >
+      <div className="w-5/6 overflow-hidden rounded-lg bg-neutral-900 px-12 md:w-2/6">
+        <div className="flex justify-end pt-6">
+          <Image
+            onClick={handleButtonClick}
+            src={roundedCloseButton}
+            height={40}
+            width={40}
+            alt="close how to play modal"
+          />
+        </div>
+        <h1 className="pb-1 text-4xl font-bold">How To Play</h1>
+        <h2 className="pb-2 text-3xl font-thin">Guess the word in 8 tries</h2>
+        <div className="mb-12 h-2 w-3/4 rounded-sm bg-green-700"></div>
+        <ul className="list-disc pb-14 pl-5 text-lg lg:text-xl">
+          <li className="pb-2">Each guess must be a valid 8 letter word.</li>
+          <li>The color of the tiles will change to show how close your guess was to the word.</li>
+        </ul>
+        <p className="pb-4 text-2xl font-bold">Examples</p>
+        <Image className="pb-2" src={modalExampleOne} alt="the word mountain with the letter t highlighted green" />
+        <p className="pb-10">
+          <b>T</b> is in the word and is in the correct place.
+        </p>
+        <Image className="pb-2" src={modalExampleTwo} alt="the word mountain with the letter t highlighted green" />
+        <p className="pb-10">
+          <b>B</b> is in the word but is in the wrong place.
+        </p>
+        <Image className="pb-2" src={modalExampleThree} alt="the word mountain with the letter t highlighted green" />
+        <p className="pb-10">
+          <b>T</b> is not in the word in any place.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PlayGuideModal;
