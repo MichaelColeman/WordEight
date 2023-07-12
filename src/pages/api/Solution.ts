0679842fec05a41f396a826ad0ea2a7e84b14c5c
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-07-11 21:51:39 -0700

    change word

--- src/pages/api/Solution.ts
+++ src/pages/api/Solution.ts
@@ -7,7 +7,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       res.status(405).json({ error: "Method not allowed" });
       return;
     }
-    res.status(200).json({ solution: "economic" });
+    res.status(200).json({ solution: "colorado" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal server error" });
