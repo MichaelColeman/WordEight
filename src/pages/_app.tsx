c98a41c7574357bc8eab9be47df4b0b97e79f779
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-07-13 13:53:38 -0700

    implement web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 /*
 The default export of _app.js is a top-level React component that wraps all the pages in your application.
@@ -8,5 +9,10 @@ or to add global styles as we're doing here.
 */
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
