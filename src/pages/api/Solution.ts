c392c234d79c8cc6f9878f76aeb0f8a4640cd45d
Tommy Coleman <michael.t.coleman7@gmail.com>
2023-07-15 09:22:25 -0700

    import prisma instance

--- src/pages/api/Solution.ts
+++ src/pages/api/Solution.ts
@@ -1,4 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from "../../../lib/prisma";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
